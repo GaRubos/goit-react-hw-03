a5c06f7a8e5d9d6ce9c8b617fb8f2a6fb2a7267e
Ruslan Garmash <garmash.rb@gmal.com>
2024-07-15 20:55:31 +0300

    add form

---
+++ src/componennts/App.jsx
@@ -0,0 +1,12 @@
+import ContactForm from "./ContactForm/ContactForm";
+
+function App() {
+  return (
+    <>
+      <h1>Домашка 3</h1>
+      <ContactForm />
+    </>
+  );
+}
+
+export default App;
---
+++ src/componennts/ContactForm/ContactForm.jsx
@@ -0,0 +1,55 @@
+import { ErrorMessage, Field, Formik, Form } from "formik";
+import * as Yup from "yup";
+
+import s from "./ContactForm.module.css";
+
+const ContactForm = () => {
+  const initialValues = {
+    name: "",
+    phone: "",
+  };
+
+  const handleSubmit = (values, actions) => {
+    console.log(values);
+    actions.resetForm();
+  };
+
+  const registerSchema = Yup.object().shape({
+    name: Yup.string()
+      .min(2, "Too Short!")
+      .max(50, "Too Long!")
+      .required("Required"),
+    phone: Yup.number()
+      .required("This field is required!")
+      .min(9, "min 9 symbol"),
+    // .max(10, "max 10 symbol"),
+  });
+
+  return (
+    <div className={s.formWrapper}>
+      <Formik
+        validationSchema={registerSchema}
+        initialValues={initialValues}
+        onSubmit={handleSubmit}
+      >
+        <Form className={s.form}>
+          <label className={s.label}>
+            <span>Name:</span>
+            <Field name="name" type="text" className={s.input} />
+            <ErrorMessage name="name" component="span" className={s.error} />
+          </label>
+
+          <label className={s.label}>
+            <span>Phone:</span>
+            <Field name="phone" type="number" className={s.input} />
+            <ErrorMessage name="phone" component="span" className={s.error} />
+          </label>
+
+          <button type="submit">Submit</button>
+        </Form>
+      </Formik>
+    </div>
+  );
+};
+
+export default ContactForm;
